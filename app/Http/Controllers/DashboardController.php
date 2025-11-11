071534b605d9e5b0251b922f773f2923d1ab78fa
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2025-11-11 21:27:23 +0100

    protect dashboard action

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function indexblog()
     {
-        $blogs = Blog::all(['id', 'title']);
+        $user = Auth::user();
+
+        $blogs = $user->blogs()->get(['id', 'title']);
 
         return view('dashboard.blog.index', compact('blogs'));
     }
 
     public function showblog(Blog $blog)
     {
+        $user = Auth::user();
+
+        if ($blog->user_id !== $user->id) {
+            abort(403, 'Unauthorized action.');
+        }
+
         $talks = $blog->talks()->get(['id', 'title', 'description']);
 
         return view('dashboard.blog.show', compact('blog', 'talks'));
