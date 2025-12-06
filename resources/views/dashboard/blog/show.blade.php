5eab8a81c4188fe365061545f8e1b1e8585a1ada
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2025-12-06 21:59:54 +0100

    merge

--- resources/views/dashboard/blog/show.blade.php
+++ resources/views/dashboard/blog/show.blade.php
@@ -3,23 +3,26 @@
 
 <main class="w-full h-full bg-background text-foreground flex flex-col gap-4 p-4 items-center justify-center overflow-hidden">
 
-    <section class="container inline-flex justify-end gap-2">
-        <x-ui.link variant="secondary" :url="route('dashboard.blog.index')">Back</x-ui.link>
-        <x-ui.link variant="secondary" :url="route('dashboard.blog.edit', $blog)">Modify</x-ui.link>
-        @if (!$blog->trashed())
-            <form action="{{ route('dashboard.blog.destroy', $blog) }}" method="POST">
-                @csrf
-                @method('DELETE')
-
-                <x-ui.button variant="warning" type="submit">Disable</x-ui.button>
-            </form>
-            @else
-            <form action="{{ route('dashboard.blog.restore', $blog) }}" method="POST">
-                @csrf
-                @method('PUT')
-                <x-ui.button variant="success" type="submit">Enable</x-ui.button>
-            </form>
-        @endif
+    <section class="container inline-flex justify-between gap-2">
+        <h2 class="text-2xl text-secondary font-semibold">{{ $blog->title }}</h2>
+        <div class="inline-flex gap-2">
+            <x-ui.link variant="secondary" :url="route('dashboard.blog.index')">Back</x-ui.link>
+            <x-ui.link variant="secondary" :url="route('dashboard.blog.edit', $blog)">Modify</x-ui.link>
+            @if (!$blog->trashed())                    
+                <form action="{{ route('blog.destroy', $blog) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    
+                    <x-ui.button variant="warning" type="submit">Disable</x-ui.button>
+                </form>
+                @else
+                <form action="{{ route('blog.restore', $blog) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <x-ui.button variant="success" type="submit">Enable</x-ui.button>
+                </form>
+            @endif
+        </div>
     </section>
 
     <section class="container flex flex-col items-center gap-2 bg-primary text-primary-foreground p-8">
@@ -30,6 +33,11 @@
             </div>
             <img class=" w-4xl" src="{{ $blog->imageUrl() }}" alt="{{$blog->slug}}">
         </div>
+    </section>
+
+    <section class="container inline-flex justify-between items-center gap-2">
+        <h2 class="text-2xl text-secondary font-semibold">Talk</h2>
+        <x-ui.link variant="secondary" :url="route('dashboard.blog.index')">Add</x-ui.link>
     </section>
 
     @foreach ($talks as $talk)
