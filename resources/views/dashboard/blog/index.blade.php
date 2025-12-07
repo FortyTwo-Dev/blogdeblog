2c58ab1b65af237cb58e1b05babe9d0a6c1a9dc7
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2025-12-07 20:33:15 +0100

    add title blog

--- resources/views/dashboard/blog/index.blade.php
+++ resources/views/dashboard/blog/index.blade.php
@@ -3,8 +3,11 @@
 
 <main class="w-full h-full bg-background text-foreground flex flex-col gap-4 p-4 items-center justify-center overflow-hidden">
 
-    <section class="container inline-flex justify-end">
-        <x-ui.link variant="secondary" :url="route('dashboard.blog.create')">Create</x-ui.link>
+    <section class="container inline-flex justify-between gap-2">
+        <h2 class="text-2xl text-secondary font-semibold">Blogs</h2>
+        <div class="inline-flex gap-2">
+            <x-ui.link variant="secondary" :url="route('dashboard.blog.create')">Create</x-ui.link>
+        </div>
     </section>
 
     @foreach ($blogs as $blog)
